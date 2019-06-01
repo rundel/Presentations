8f9324eeb32568da2015dbe1ded11fc3ffdf9d0e
Colin Rundel <rundel@gmail.com>
2019-06-01 16:08:35 -0700

    Stuff

--- Duke 2018 - DVS/live_demos/darksky.R
+++ Duke 2018 - DVS/live_demos/darksky.R
@@ -3,4 +3,51 @@
 library(jsonlite)
 library(purrr)
 library(dplyr)
+library(ggplot2)
+
+
+durham = jsonlite::fromJSON(
+  "https://api.darksky.net/forecast/24e13b35a014e3ca53a36a217243f61d/35.9940,-78.8986",
+  simplifyDataFrame = FALSE
+)
+
+str(durham, max.level=1)
+
+str(durham$minutely, max.level=1)
+str(durham$minutely$data[[1]], max.level=1)
+
+minutely = data_frame(
+  time                 = map_int(durham$minutely$data, "time"),
+  precipIntensity      = map_dbl(durham$minutely$data, "precipIntensity"),
+  precipIntensityError = map_dbl(durham$minutely$data, "precipIntensityError"),
+  precipProbability    = map_dbl(durham$minutely$data, "precipProbability"),
+  precipType           = map_chr(durham$minutely$data, "precipType")
+)
+
+ggplot(minutely, aes(x=time, y=precipIntensity)) + geom_line()
+
+
+
+str(durham$hourly$data[[1]], max.level=1)
+
+## Generalization
+
+map(durham$minutely$data, as_data_frame) %>% bind_rows()
+
+data = list(
+  currently = as_data_frame(durham$currently),
+  minutely = map_dfr(durham$minutely$data, as_data_frame),
+  hourly = map_dfr(durham$hourly$data, as_data_frame),
+  daily = map_dfr(durham$daily$data, as_data_frame)
+) %>% 
+  map(
+    function(tbl) {
+      tbl %>% mutate_at(
+        vars(contains("time")),
+        as.POSIXct,
+        tz = durham$timezone, origin = "1970-1-1"
+      )
+    }
+  )
+
 
--- Duke 2018 - DVS/live_demos/dennys.R
+++ Duke 2018 - DVS/live_demos/dennys.R
@@ -5,4 +5,61 @@ library(dplyr)
 library(purrr)
 library(stringr)
 
+## Restaraunt level data
 
+url = "https://locations.dennys.com/NC/DURHAM/248848"
+
+get_restaraunt_data = function(url) {
+  page = read_html(url)
+  
+  data_frame(
+    name = page %>% html_nodes("#location-name") %>% html_text(),
+    address = page %>% html_nodes(".c-address-street-1") %>% html_text(),
+    city = page %>% html_nodes(".c-address-city span") %>% html_text(),
+    state = page %>% html_nodes(".c-address-state") %>% html_text(),
+    zipcode = page %>% html_nodes(".c-address-postal-code") %>% html_text(),
+    telephone = page %>% html_nodes("#telephone") %>% html_text()
+  ) 
+}
+
+get_restaraunt_data("https://locations.dennys.com/NC/DURHAM/248848")
+get_restaraunt_data("https://locations.dennys.com/NC/BATTLEBORO/246171")
+
+
+## State level data
+
+get_city_links = function(url) {
+  page = read_html(url)
+  
+  page %>% html_nodes(".c-location-grid-item-title a") %>% 
+    html_attr("href") %>% file.path(dirname(url), .)
+}
+
+get_state_data = function(url) {
+  
+  page = read_html(url)
+  
+  base_url = dirname(url)
+  
+  state_links = page %>% html_nodes(".c-directory-list-content-item-link") %>% 
+    html_attr("href") %>% file.path(base_url, .)
+  
+  rest_links = state_links[str_detect(state_links, "[0-9]{4,}$")]
+  
+  city_links = state_links[!str_detect(state_links, "[0-9]{4,}$")] %>%
+    lapply(get_city_links) %>%
+    unlist()
+  
+  
+  lapply(
+    c(rest_links, city_links), 
+    function(link) {
+      print(link)
+      get_restaraunt_data(link)
+    }
+  ) %>%
+    bind_rows()
+}
+
+
+get_state_data("https://locations.dennys.com/NC")
