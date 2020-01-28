9fbdfcbafff9d9799d54cfae1323e76d2aad938d
Colin Rundel <rundel@gmail.com>
2020-01-28 12:39:59 -0800

    Update

--- ECME 2019/exercise1.R
+++ ECME 2019/exercise1.R
@@ -1,2 +1,27 @@
-# 1. How many flights to Los Angeles (LAX) did each of the 
-# legacy carriers (AA, UA, DL or US) have in May from JFK?
+library(dplyr)
+library(nycflights13)
+
+# 1. How many flights from New York (JFK) to Los Angeles (LAX) \
+# did each carrier have in May?
+
+flights %>%
+  filter(origin == "JFK") %>%
+  filter(dest == "LAX") %>%
+  filter(month == 5) %>%
+  group_by(carrier) %>%
+  summarise(n =)
+
+flights %>%
+  filter(origin == "JFK") %>%
+  filter(dest == "LAX") %>%
+  filter(month == 5) %>%
+  count(carrier)
+
+
+flights %>%
+  filter(origin == "JFK", dest == "LAX", month == 5) %>%
+  count(carrier)
+
+flights %>%
+  filter(origin == "JFK", dest == "LAX", month == 5) %>%
+  count(carrier)
